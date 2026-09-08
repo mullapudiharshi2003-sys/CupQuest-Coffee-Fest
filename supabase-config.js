968107067ced78a63b6ec49d8cf8a50ec04f21e2
oopsitworks-git <mullapudi.harshi2003@gmail.com>
2026-09-08 15:32:54 +0530

    Fix Supabase connection

---
+++ supabase-config.js
@@ -0,0 +1,10 @@
+var SUPABASE_URL =
+  "https://dhuvyehchxcziodhtjcd.supabase.co";
+
+var SUPABASE_PUBLISHABLE_KEY =
+  "sb_publishable_z-5bpCobS846lBpP5RFsmQ_J__eFGw9";
+
+var supabaseClient = window.supabase.createClient(
+  SUPABASE_URL,
+  SUPABASE_PUBLISHABLE_KEY
+);
